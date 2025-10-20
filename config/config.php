982cf8b736b6d0dc7108c7a7b35450b228cc8838
P4poule <151908229+P4poule@users.noreply.github.com>
2025-10-20 16:11:12 +0200

    Delete config directory

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-
-// Définition de constantes pour la configuration de la base de données
-define('DB_HOST', 'localhost');  // Hôte de la base de données (généralement localhost)
-define('DB_NAME', 'postgres');  // Nom de la base de données
-define('DB_USER', 'pizza');  // Nom d'utilisateur de la base de données
-define('DB_PASS', 'sushi');  // Mot de passe de la base de données
-define('DB_PORT', '5432');  // Mot de passe de la base de données
